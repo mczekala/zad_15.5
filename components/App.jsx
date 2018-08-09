de8d24a6884bbc004a6011ca003fe9404197eb1f
Marcin Czekala <marcin_czekala@icloud.com>
2018-08-09 18:13:23 +0200

    first commit

---
+++ components/App.jsx
@@ -0,0 +1,74 @@
+App = React.createClass({
+  getInitialState() {
+    return {
+      loading: false,
+      searchingText: "",
+      gif: {}
+    };
+  },
+
+  getGif: function(searchingText, callback) {
+    return new Promise(function(resolve, reject) {
+      var url =
+        "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" +
+        searchingText;
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var data = JSON.parse(xhr.responseText).data;
+          var gif = {
+            url: data.fixed_width_downsampled_url,
+            sourceUrl: data.url
+          };
+          // callback(gif);
+          resolve(gif);
+        } else {
+          reject(new Error(this.statusText));
+        }
+      };
+      xhr.onerror = function() {
+        reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+      };
+      xhr.send();
+    });
+  },
+
+  handleSearch: function(searchingText) {
+    this.setState({
+      loading: true
+    });
+
+    this.getGif(searchingText)
+      .then(gif => {
+        this.setState({
+          loading: false,
+          gif: gif,
+          searchingText: searchingText
+        });
+      })
+      .catch(error => console.log(error));
+  },
+  render: function() {
+    var styles = {
+      margin: "0 auto",
+      textAlign: "center",
+      width: "90%"
+    };
+    return (
+      <div style={styles}>
+        <h1> Wyszukiwarka GIFow! </h1>
+        <p>
+          Znajdź gifa na <a href="http://giphy.com"> giphy </a>. Naciskaj enter,
+          aby pobrać kolejne gify.
+        </p>
+        <Search onSearch={this.handleSearch} />
+        <Gif
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+        />
+      </div>
+    );
+  }
+});
